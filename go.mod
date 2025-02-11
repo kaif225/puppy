8a98b9c3d637a45de9a551fa1ca367b6e9281a2d
kaif225 <kaifuusmani@gmail.com>
2025-02-11 15:41:07 +0530

    added puppy.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kaif225/puppy
+
+go 1.23.6
---
+++ puppy.go
@@ -0,0 +1,14 @@
+/*
+First we did go mod init repo path
+Then created this puupy.go and wroe the basic code
+*/
+
+package puppy
+
+func Bark() string {
+	return "woof"
+}
+
+func Barks() string {
+	return "woof! woof woof!"
+}
